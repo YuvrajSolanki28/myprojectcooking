c393ba7f09b6f7c499c1f1f12f84ae68f410d668
yuvraj <yuvrajsolanki2809@gmail.com>
2024-08-30 23:13:50 +0530

    2

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import { data } from '../data/data.js';
 
 const Food = () => {
-  //   console.log(data);
   const [foods, setFoods] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedFood, setSelectedFood] = useState(null);
 
-  //   Filter Type burgers/pizza/etc
+  const handleClick = (food) => {
+    setSelectedFood(food);
+    setShowModal(!showModal);
+  };
+
   const filterType = (category) => {
     setFoods(
       data.filter((item) => {
@@ -14,19 +19,17 @@ const Food = () => {
     );
   };
 
-
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
       <h1 className='text-orange-600 font-bold text-4xl text-center'>
-        Top Rated Menu Items
+        Top Rated Items
       </h1>
 
       {/* Filter Row */}
       <div className='flex flex-col lg:flex-row justify-between'>
-        {/* Fliter Type */}
         <div>
           <p className='font-bold text-gray-700'>Filter Type</p>
-          <div className='flex justfiy-between flex-wrap'>
+          <div className='flex justify-between flex-wrap'>
             <button
               onClick={() => setFoods(data)}
               className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'
@@ -69,15 +72,44 @@ const Food = () => {
             />
             <div className='flex justify-between px-2 py-4'>
               <p className='font-bold'>{item.name}</p>
-              <p>
-                <span className='bg-orange-500 text-white p-1 rounded-full'>
-                  {item.price}
-                </span>
-              </p>
+              <button
+                onClick={() => handleClick(item)}
+                className='bg-orange-500 text-white hover:bg-white hover:text-orange-500 p-1 rounded-full font-bold px-2'
+              >
+                Recipe
+              </button>
             </div>
           </div>
         ))}
       </div>
+
+      {showModal && selectedFood && (
+       <div className="fixed inset-0 z-50 overflow-x-hidden overflow-y-auto flex justify-center items-center backdrop-blur-sm">
+       <div className="md:w-2/3 mt-24 w-4/5 bg-white p-2 rounded-lg shadow-lg">
+         <div className="flex justify-center mt-4">
+           <img src={selectedFood.image} alt={selectedFood.name} className="rounded-full object-cover w-3/12" />
+         </div>
+         <div className="py-6 m-2">
+           <h2 className="text-center">{selectedFood.name}</h2>
+           <h2>Instructions:</h2>
+           <p className="text-center text-sm px-2 mt-2">
+             {selectedFood.instructions}
+           </p>
+           <div className="flex justify-end mt-4">
+           <a href ={selectedFood.video} className="shadow-lg p-2 rounded-md text-md m-2 hover:bg-neutral-300" rel="noopener noreferrer" target="_blank">Watch Video</a>
+             <button
+               onClick={() => setShowModal(false)}
+               className="hover:bg-neutral-300 shadow-lg p-2 rounded-md text-md m-2"
+             >
+               Close
+             </button>
+           </div>
+         </div>
+       </div>
+     </div>
+     
+      
+      )}
     </div>
   );
 };
--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,12 @@
 export const data = [
     {
       id: 1,
-      name: 'Double Cheeseburger',
+      name: 'Samosa',
       category: 'burger',
+      instructions: 'What makes this the best homemade samosa recipe? My foolproof instructions will guarantee that your potatoes and peas samosa are hearty with a perfectly flaky, crispy crust I use a bit less water than some other recipes to make my dough, which creates an ever flakier and crave-able samosa crust Plus, the filling in these Punjabi Samosa is just fantastic. It’s super easy to prepare in a stovetop pressure cooker or using an Instant Pot and in a pan on the stove Green peas and potatoes are blended with classic Indian spices for a warm, satisfying stuffing that will keep you wanting more With a perfect crust and comforting filling, this recipe is a real winner! Give it a try and you’ll be amazed at how simple and fun it is to make samosa recipe from scratch at home Our personal favorite is and will always be a Punjabi Samosa which is what this recipe is all about. The classic potato and green peas stuffing in this recipe is adapted from my cooking school notes I have also made a video which shows the shaping, stuffing and deep frying the Aloo Samosa. You can have a look at the video, if the photos don’t help',
+      video:'https://youtu.be/cN7paRwBbrY?si=hCk7FDNqaiRctCYc',
       image:
-        'https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+        'https://images.pexels.com/photos/2474658/pexels-photo-2474658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       
     },
     {
@@ -12,7 +14,7 @@ export const data = [
       name: 'Bacon Cheeseburger',
       category: 'burger',
       image:
-        'https://images.unsplash.com/photo-1553979459-d2229ba7433b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+        'https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       
     },
     {
@@ -20,23 +22,27 @@ export const data = [
       name: 'Mushroom Burger',
       category: 'burger',
       image:
-        'https://images.unsplash.com/photo-1608767221051-2b9d18f35a2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJ1cmdlcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=1400&q=60',
+        'https://images.pexels.com/photos/115740/pexels-photo-115740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       
     },
     {
       id: 4,
-      name: 'Loaded Burger',
-      category: 'burger',
+      name: 'Chana Masala',
+      category: 'indian',
+      instructions: 'Chana masala or chickpea curry, often called Amritsari Chole, originates from Punjab but is popular all over India. There are many versions of this dish and every house has its own take on it. This super authentic recipe from my mother-in-law’s kitchen is my favorite, with the chickpeas smothered in a delicious thick, piquant and spiced gravy. First, they are cooked and infused with black tea and black cardamom, then simmered with a masala made with supremely aromatic roasted spices. This dish gets its tang from dried pomegranate seeds and raw mango powder. Though it’s a little bit of work, it is the real McCoy and once you’ve had this version, nothing else will live up to it!',
+      video:'https://www.youtube.com/watch?v=uNlriiDr2JM&pp=ygUTQ2hhbmEgTWFzYWxhIHJlY2lwZQ%3D%3D',
       image:
-        'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60',
+        'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/25/0/FNK_Chana-Masala_H2_s4x3.jpg.rend.hgtvcom.791.594.suffix/1614271965516.jpeg',
       
     },
     {
       id: 5,
-      name: 'Feta & Spinnach',
+      name: 'Aloo Matar Tikki',
       category: 'pizza',
+      instructions: 'Aloo tikki -- or potato patties -- were a staple at my house growing up and my mother’s favorite snack. Stuffed with peas on festive occasions, these patties are best eaten hot, straight from the skillet to the plate. We loved eating these crispy and deliciously savory treats with a piquant cilantro chutney. On the streets in Delhi they are deep fried in oil and served on a plate made of dried leaves. Dredged in breadcrumbs, the patties in this recipe are as crispy as the ones from the streets, and way healthier. Since I can never get enough of the pea filling in the patties, I decided to add peas to the cilantro chutney, which is completely inauthentic but is even better than the real thing',
+      video:'https://www.youtube.com/watch?v=Om8PEswhlpw&pp=ygUXQWxvbyBNYXRhciBUaWtraSByZWNpcGU%3D',
       image:
-        'https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
+        'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/11/06/FNK_Aloo-Matar-Tikki_H1.jpg.rend.hgtvcom.826.620.suffix/1604695275529.webp',
       
     },
     {
@@ -94,37 +100,6 @@ export const data = [
       image:
         'https://images.unsplash.com/photo-1564093497595-593b96d80180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJ1aXQlMjBzYWxhZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
       
-    },
-    {
-      id: 13,
-      name: 'Wings',
-      category: 'chicken',
-      image:
-        'https://images.unsplash.com/photo-1567620832903-9fc6debc209f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      
-    },
-    {
-      id: 14,
-      name: 'Baked Chicken',
-      category: 'chicken',
-      image:
-        'https://images.unsplash.com/photo-1594221708779-94832f4320d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hpY2tlbiUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-      
-    },
-    {
-      id: 15,
-      name: 'Chicken Tenders',
-      category: 'chicken',
-      image:
-        'https://images.unsplash.com/photo-1562967914-608f82629710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
-      
-    },
-    {
-      id: 16,
-      name: 'Chicken Kabob',
-      category: 'chicken',
-      image:
-        'https://images.unsplash.com/photo-1603360946369-dc9bb6258143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNoaWNrZW4lMjBmb29kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
     },
   ];
   
